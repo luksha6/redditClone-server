b163fe16e282d9eb931ab758d089be0b7908d6c9
Luka <l.bjelica@globalsoft.ba>
2021-02-03 23:07:41 +0100

    Voting transaction

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -39,25 +39,40 @@ export class PostResolver {
         const isUpdoot = value !== -1;
         const realValue = isUpdoot ? 1 : -1;
         const { userId } = req.session;
-        await Updoot.insert({
-            userId,
-            postId,
-            value: realValue,
-        })
 
-        getConnection().query(`
-            START TRANSACTION
+        const updoot = await Updoot.findOne({
+            where: { postId, userId }
+        });
+        
+        if (updoot && updoot.value !== realValue) {
+            await getConnection().transaction(async tm => {
+                await tm.query(`
+                update updoot
+                set value = $1
+                where "postId" = $2 and "userId" = $3
+                `, [realValue, postId, userId]);
 
-            insert into updoot("userId", "postId", value)
-            values ($1, $2, $3)
+                await tm.query(`
+                update post
+                set points = points + $1
+                where id = $2
+                `, [2 * realValue, postId]);
+            });
+        } else if (!updoot) {
+            await getConnection().transaction(async tm => {
+                await tm.query(`
+                insert into updoot ("userId", "postId", value)
+                values ($1, $2, $3)
+                `, [userId, postId, realValue]);
 
-            update post
-            set points = points + $4
-            where post.id = $5
-            
-            COMMIT
-        `, [userId, postId, realValue, realValue, postId]);
-
+                await tm.query(`
+                update post
+                set points = points + $1
+                where id = $2
+                `, [realValue, postId]);
+            });
+        }
+    
         return true;
     }
 
